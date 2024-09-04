3702beb19d0a84125081ab36ab61333f08778cc8
Son Nguyen <hoangson091104@gmail.com>
2024-09-04 13:57:03 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# SQLite Database URL
+SQLALCHEMY_DATABASE_URL = "sqlite:///./bookstore.db"
+
+# IMPORTANT: Replace these details with your own configuration!
